c00dd0c5843fec7621b3df96025f357ca1e6d035
yasuo kurihara <breitling1216@gmail.com>
2022-06-29 17:14:36 +0900

    👕: コードスタイルの修正

--- src/components/sample/elements/Footer/Footer.test.tsx
+++ src/components/sample/elements/Footer/Footer.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+
 import { Footer } from './Footer';
 
 /**
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import reportWebVitals from './utils/reportWebVitals';
 import '@public/styles/index.css';
