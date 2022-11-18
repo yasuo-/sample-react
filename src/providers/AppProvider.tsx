ac651d64a6aae1a7b7b6682461fb485f1c3c7206
yasuo kurihara <breitling1216@gmail.com>
2022-11-18 16:30:10 +0900

    Add: Recoil & use suspense

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import { ThemeProvider, Theme } from '@aws-amplify/ui-react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from '@tanstack/react-query';
 import { ErrorBoundary } from 'react-error-boundary';
 import { HelmetProvider, HelmetData } from 'react-helmet-async';
 import { I18nextProvider } from 'react-i18next';
+import { RecoilRoot } from 'recoil';
 
 import { ErrorFallback } from '@/components/error';
 import i18n from '@/lib/locale/i18n';
@@ -22,7 +23,13 @@ const theme: Theme = {
 };
 
 // Create a client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 const helmetContext = new HelmetData({});
 
 /**
@@ -36,7 +43,11 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
       <QueryClientProvider client={queryClient}>
         <HelmetProvider context={helmetContext}>
           <ThemeProvider theme={theme}>
-            <ErrorBoundary FallbackComponent={ErrorFallback}>{children}</ErrorBoundary>
+            <RecoilRoot>
+              <QueryErrorResetBoundary>
+                <ErrorBoundary FallbackComponent={ErrorFallback}>{children}</ErrorBoundary>
+              </QueryErrorResetBoundary>
+            </RecoilRoot>
           </ThemeProvider>
         </HelmetProvider>
       </QueryClientProvider>
