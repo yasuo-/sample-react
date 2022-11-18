0adb495eafafc0e91a31ae1391a4103432a86ce9
yasuo kurihara <breitling1216@gmail.com>
2022-11-18 09:45:20 +0900

    update package

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,16 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+  },
   resolve: {
     alias: {
       '@/': `${__dirname}/src/`,
       '@public/': `${__dirname}/public/`,
+      './runtimeConfig': './runtimeConfig.browser',
     },
   },
 });
